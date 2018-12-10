23d2aae128dae07f1f6349ccde4a61357eeeec78
Muhammad Tahir <m.tahir@aqtsoft.com>
2018-12-10 21:42:33 +0500

    Update HomeModel.php

--- application/models/HomeModel.php
+++ application/models/HomeModel.php
@@ -6,6 +6,7 @@ class HomeModel extends CI_Model
     public function getBlocsGame($getOrder, $getPag)
     {
         $getPag = $getPag*(int)$this->config->item('home_pag')-(int)$this->config->item('home_pag');
+        echo $getPag."<br/>";
         if($getOrder === 'rated') {
             $sql = "SELECT id, title, url, id_category, played, note, image, date_upload,video_url FROM 2d_games WHERE status = 1 and display_home=1 GROUP BY id ORDER BY note DESC LIMIT ?,?";
         } elseif($getOrder === 'news') {
@@ -20,7 +21,7 @@ class HomeModel extends CI_Model
             $sql = "SELECT id, title, url, id_category, played, note, image, date_upload,video_url FROM 2d_games WHERE status = 1 and display_home=1 GROUP BY id ORDER BY title LIMIT ?,?";
         }
         $query = $this->db->query($sql, array((int)$getPag, (int)$this->config->item('home_pag')));
-        echo $this->db->last_query($query);
+        //echo $this->db->last_query($query);
         $nbRows = $this->db->count_all('2d_games');
         $getBlocGame = '';
         if($query->result()){
