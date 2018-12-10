7a5cec45aab62ded33dba6be617e31bcfeea252d
Muhammad Tahir <m.tahir@aqtsoft.com>
2018-12-10 13:18:50 +0500

    Home Page
    if no game found

--- application/models/HomeModel.php
+++ application/models/HomeModel.php
@@ -22,34 +22,39 @@ class HomeModel extends CI_Model
         $query = $this->db->query($sql, array((int)$getPag, (int)$this->config->item('home_pag')));
         $nbRows = $this->db->count_all('2d_games');
         $getBlocGame = '';
-        foreach ($query->result() as $row) {
-            // Comparison of dates for displaying the new tab on the game
-            $date_upload = date_parse($row->date_upload);
-            $datetime1 = date_create($date_upload['year'].'-'.$date_upload['month'].'-'.$date_upload['day']);
-            $datetime2 = date_create(date("Y-m-d"));
-            $interval = date_diff($datetime1, $datetime2);
-            $time = $interval->format('%a');
-            $classShow = ($time<=90)?'show':'';
-            $getBlocGame .= '<div class="col-sm-12  col-md-2 col-lg-game-'.$this->config->item('home_nb').' p-b-20">
-                                <div class="game-list-box">
-    								<a href="'.site_url('game/show/'.$row->url).'/" class="image-popup" title="'.$row->title.'">
-                                        <video autoplay loop muted playsinline>
-                                            <source src="'.$row->video_url.'" type="video/mp4">
-                                        </video>
-    									<img src="'.(empty($row->image) ? site_url('assets/images/default_swf.jpg') : $row->image).'" class="thumb-img" alt="work-thumbnail">
-    								</a>
+        if($query->result()){
+            foreach ($query->result() as $row) {
+                // Comparison of dates for displaying the new tab on the game
+                $date_upload = date_parse($row->date_upload);
+                $datetime1 = date_create($date_upload['year'].'-'.$date_upload['month'].'-'.$date_upload['day']);
+                $datetime2 = date_create(date("Y-m-d"));
+                $interval = date_diff($datetime1, $datetime2);
+                $time = $interval->format('%a');
+                $classShow = ($time<=90)?'show':'';
+                $getBlocGame .= '<div class="col-sm-12  col-md-2 col-lg-game-'.$this->config->item('home_nb').' p-b-20">
+                                    <div class="game-list-box">
+                                        <a href="'.site_url('game/show/'.$row->url).'/" class="image-popup" title="'.$row->title.'">
+                                            <video autoplay loop muted playsinline>
+                                                <source src="'.$row->video_url.'" type="video/mp4">
+                                            </video>
+                                            <img src="'.(empty($row->image) ? site_url('assets/images/default_swf.jpg') : $row->image).'" class="thumb-img" alt="work-thumbnail">
+                                        </a>
 
-    								<!--<div class="game-action '.$classShow.'">
-    									<a href="'.site_url('news/').'" class="btn btn-warning btn-sm">New</a>
-    								</div>-->
+                                        <!--<div class="game-action '.$classShow.'">
+                                            <a href="'.site_url('news/').'" class="btn btn-warning btn-sm">New</a>
+                                        </div>-->
 
-    								'.rating($this->getNote($row->id), 'game-rating').'
+                                        '.rating($this->getNote($row->id), 'game-rating').'
 
-    								<div class="game-title">
-    									<h2 class="h5"><a href="'.site_url('game/show/'.$row->url).'" title="'.$row->title.'">'.mb_strimwidth($row->title, 0, 17, '...').'</a> </h2>
-    								</div>
-                                </div>
-							</div>';
+                                        <div class="game-title">
+                                            <h2 class="h5"><a href="'.site_url('game/show/'.$row->url).'" title="'.$row->title.'">'.mb_strimwidth($row->title, 0, 17, '...').'</a> </h2>
+                                        </div>
+                                    </div>
+                                </div>';
+            }
+        }
+        else{
+            $getBlocGame .= '<div class="col-sm-12  col-md-2 col-lg-game-'.$this->config->item('home_nb').' p-b-20">No game found!</div>';
         }
         return array(
          'getBlocGame' => $getBlocGame,
