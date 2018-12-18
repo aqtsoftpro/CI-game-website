d4f33389de088f26bc9c324e5672ba5779d2a6f1
Muhammad Tahir <m.tahir@aqtsoft.com>
2018-12-18 17:33:05 +0500

    One Mouse Over start video from start

--- assets/js/jquery.app.js
+++ assets/js/jquery.app.js
@@ -338,11 +338,11 @@ $(document).ready(function() {
     });
     $(document).on('mouseover','.game-list-box img', function(e){
       $(this).parent().next().slideToggle();
+        $(this).parent().find('video').load();
         $(this).parent().find('video').show();
     });
     $(document).on('mouseleave','.game-list-box', function(e){
       $(this).parent().find('video').hide();
-      $(this).parent().find('video').load();
     });
     $('#logo_submit').submit(function(e){
       e.preventDefault(); 
