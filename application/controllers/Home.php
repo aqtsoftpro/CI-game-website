eb21a9e1fc58210235e7ade03d7303e6ca3fcd77
zahoor1989 <43780644+zahoor1989@users.noreply.github.com>
2018-12-25 14:35:42 +0500

    Update Home.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -42,13 +42,10 @@ class Home extends CI_Controller
     public function index()
     {
         $data['tags'] = $this->KeywordsModel->keywords_tags();
+        $search = $_REQUEST['q'];
+        $getOrder = $this->uri->segment(1);
         $data['getBlocGame'] = $this->homeModel->getBlocsGame($getOrder, 1,$search);
         $content = $this->load->view('front/index',$data,true);
-        $search = $_REQUEST['q'];       
-        
-        $getOrder = $this->uri->segment(1);
-        
-    
         $this->load->view('front/template', array('content' => $content));
 
         // var_dump($this->input->cookie());
