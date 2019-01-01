18d2878b987ac92425746ac566585c5cac32d0b8
Muhammad Tahir <m.tahir@aqtsoft.com>
2019-01-01 21:27:34 +0500

    comments divs styling

--- application/models/GameModel.php
+++ application/models/GameModel.php
@@ -407,8 +407,8 @@ class GameModel extends CI_Model
 //							'.$related1.'
 //						</div>';
             $comment = $row1->comment;
-                    if(strlen($comment)>60){
-                        $comment = substr($comment,0,60).'...';
+                    if(strlen($comment)>50){
+                        $comment = substr($comment,0,50).'...';
                     }
             $getComs .= '<div class="col-sm-4">
                                     <div class="panel panel-default com-panel">                                       
